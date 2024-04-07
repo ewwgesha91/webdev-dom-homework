bfbc3051c547d0ae6c55311b1379e242ed8fa989
ewwgesha91 <ewwgesha@mail.ru>
2024-04-07 18:14:32 +0200

    hw17

--- API.js
+++ API.js
@@ -1,7 +1,7 @@
 import { getComments } from "./main.js";
 
-const host = 'https://wedev-api.sky.pro/api/v2/evgeniia-lomakina/comments';
-const userUrl = 'https://wedev-api.sky.pro/api/user/login';
+const host = "https://wedev-api.sky.pro/api/v2/evgeniia-lomakina/comments";
+const userUrl = "https://wedev-api.sky.pro/api/user/login";
 
 export let token;
 
@@ -11,73 +11,75 @@ export const setToken = (newToken) => {
 
 export let user;
 export const setUser = (newUser) => {
-  user = newUser.trim().replaceAll('>', '&gt;').replaceAll('<', '&lt;');
-}
+  user = newUser.trim().replaceAll(">", "&gt;").replaceAll("<", "&lt;");
+};
 
 export function get() {
-    return fetch(host, {
+  return fetch(host, {
     method: "GET",
     headers: {
       Authorization: `Bearer ${token}`,
-    }
+    },
   }).then((response) => {
     if (response.status === 500) {
-      throw new Error('Сервер сломался');
+      throw new Error("Сервер сломался");
     } else {
       return response.json();
     }
-  })
-};
+  });
+}
 
-export function post({name, text}) {
-    return fetch(host, {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-      body: JSON.stringify({
+export function post({ name, text }) {
+  return fetch(host, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({
       name: name,
       text: text,
-    //   forceError: true,
+      //   forceError: true,
     }),
   }).then((response) => {
     if (response.status === 400) {
-      throw new Error('Некорректный запрос');
+      throw new Error("Некорректный запрос");
     } else if (response.status === 500) {
-      throw new Error('Сервер сломался');
+      throw new Error("Сервер сломался");
     } else {
       return getComments();
     }
-    });
-};
+  });
+}
 
-export function login({login, password}) {
+export function login({ login, password }) {
   return fetch(userUrl, {
     method: "POST",
     body: JSON.stringify({
-    login,
-    password,
-  }),
-}).then((response) => {
-  if (response.status === 201) {
-    return response.json();
-  };
-  if (response.status === 400) {
-    throw new Error('Некорректные логин или пароль');
-  };
-  if (response.status === 500) {
-    throw new Error('Ошибка сервера');
-  };
-  }). catch((error) => {
-    if (error.message === 'Некорректные логин или пароль') {
-      alert('Некорректно введены логин или пароль.');
-    };
-    if (error.message === 'Ошибка сервера') {
-      alert('Сервер сломался, попробуйте позже.');
-    }
-    if (error.message === 'Failed to fetch') {
-      alert('Проблемы с соединением, попробуйте позже.');
-    }
-    console.warn(error);
+      login,
+      password,
+    }),
   })
-};
+    .then((response) => {
+      if (response.status === 201) {
+        return response.json();
+      }
+      if (response.status === 400) {
+        throw new Error("Некорректные логин или пароль");
+      }
+      if (response.status === 500) {
+        throw new Error("Ошибка сервера");
+      }
+    })
+    .catch((error) => {
+      if (error.message === "Некорректные логин или пароль") {
+        alert("Некорректно введены логин или пароль.");
+      }
+      if (error.message === "Ошибка сервера") {
+        alert("Сервер сломался, попробуйте позже.");
+      }
+      if (error.message === "Failed to fetch") {
+        alert("Проблемы с соединением, попробуйте позже.");
+      }
+      console.warn(error);
+    });
+}
--- main.js
+++ main.js
@@ -1,66 +1,73 @@
 "use strict";
-import { get } from "./API.js";
+import { get, token } from "./API.js";
 import { renderLogin } from "./renderLogin.js";
 import { renderComments, addNewComment } from "./render.js";
+import { format } from "date-fns";
 
-const preloader = document.getElementById('preloader');
+const preloader = document.getElementById("preloader");
 
-export let comments = [ ];
+export let comments = [];
 
 export const getComments = () => {
-  get().then((responseData) => {
-       const appComments = responseData.comments.map((comment) => {
+  get()
+    .then((responseData) => {
+      const appComments = responseData.comments.map((comment) => {
+        const createDate = format(new Date(), "yyyy-MM-dd hh.mm.ss");
         return {
           name: comment.author.name,
-          date: new Date(comment.date),
+          date: createDate,
           text: comment.text,
           likesCounter: comment.likes,
           isLiked: comment.isLiked,
         };
       });
       comments = appComments;
-      preloader.style.display = 'none';
-      renderComments({comments});
+      preloader.style.display = "none";
+      renderComments({ comments });
       quoteComment();
     })
     .catch((error) => {
-       if (error.message === 'Сервер сломался') {
-        alert('Сервер сломался, попробуйте позже.');
-      };
-      if (error.message === 'Failed to fetch') {
-        alert('Проблемы с соединением, попробуйте позже.');
+      if (error.message === "Сервер сломался") {
+        alert("Сервер сломался, попробуйте позже.");
+      }
+      if (error.message === "Failed to fetch") {
+        alert("Проблемы с соединением, попробуйте позже.");
       }
       console.warn(error);
     });
-}; 
+};
 getComments();
 
 renderLogin();
 
 export const initLikesListeners = () => {
-  const likeButtons = document.querySelectorAll('.like-button');
+  const likeButtons = document.querySelectorAll(".like-button");
   for (const likeButton of likeButtons) {
-    likeButton.addEventListener('click', (event) => {
+    likeButton.addEventListener("click", (event) => {
       event.stopPropagation();
+      if (!token) {
+        alert("Пожалуйста, зарегестрируйтесь или войдите в аккаунт");
+        return;
+      }
       const index = likeButton.dataset.index;
       comments[index].likesCounter += comments[index].isLiked ? -1 : +1;
       comments[index].isLiked = !comments[index].isLiked;
-      renderComments({comments});
+      renderComments({ comments });
       quoteComment();
-    })
+    });
   }
-}
+};
 
 export const quoteComment = () => {
-    const commentElements = document.querySelectorAll(".comment");
-    for (const comment of commentElements) {
-      comment.addEventListener('click', () => {
-        const textInput = document.querySelector('.add-form-text');
-        const index = comment.dataset.index;
-        textInput.value = `${comments[index].name}: \n${comments[index].text}\n`;
-      })
-    }
+  const commentElements = document.querySelectorAll(".comment");
+  for (const comment of commentElements) {
+    comment.addEventListener("click", () => {
+      const textInput = document.querySelector(".add-form-text");
+      const index = comment.dataset.index;
+      textInput.value = `${comments[index].name}: \n${comments[index].text}\n`;
+    });
   }
+};
 
-renderComments({comments}); 
+renderComments({ comments });
 quoteComment();
